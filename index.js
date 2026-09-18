c8189dc674d602a31e25b0e7b4fe6a64368d4848
Willian Chua <willianchua.wc@gmail.com>
2026-09-18 17:11:43 +0800

    Hardcode port for railway fix attempt

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server listening at port ${PORT}`)
 })
